4b0e13d6aa4d8744eb4a78c7de39b3df58581fa8
Rami Smat <113128219+RamiSmat@users.noreply.github.com>
2024-01-12 14:35:25 +0100

    Delete BibliothequeEJBB directory

--- BibliothequeEJBB/ejbModule/com/BibliothequeEJB/dao/interfaces/LivreDao.java
+++
@@ -1,9 +0,0 @@
-//package com.BibliothequeEJB.dao.interfaces;
-//
-//import javax.ejb.Local;
-//
-//import com.BibliothequeEJB.entities.Livre;
-//@Local
-//public interface LivreDao {
-//	public void ajouterLivre(Livre livre);
-//}
