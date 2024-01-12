8ae3f6a37d2bf345363f68f08b928304d027e08e
Rami Smat <113128219+RamiSmat@users.noreply.github.com>
2024-01-12 14:35:33 +0100

    Delete BibliothequeWEB directory

--- BibliothequeWEB/src/main/java/Service/WebApp.java
+++
@@ -1,9 +0,0 @@
-package Service;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/bibliotheque/ouvrages")
-public class WebApp extends Application{
-
-}
